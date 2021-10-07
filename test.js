f14f6a825bf535fc5d0e8e6e0c94168a0ba53c70
German Heinz <gerheinz@everis.com>
2021-10-07 20:29:48 +0200

    primer commit 1

---
+++ test.js
@@ -0,0 +1,10 @@
+
+
+const texto =" de prueba 1";
+const texto =" de prueba 2";
+const texto =" de prueba 3";
+const texto =" de prueba 4";
+const texto =" de prueba 5";
+const texto =" de prueba 6";
+
+
